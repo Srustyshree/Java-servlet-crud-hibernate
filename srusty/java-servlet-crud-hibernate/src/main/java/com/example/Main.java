22106d37bc1b137898246ee172b6a3b7d577ee9c
Srustyshree <141812491+Srustyshree@users.noreply.github.com>
2025-03-31 22:02:06 +0530

    Main.java

--- srusty/java-servlet-crud-hibernate/src/main/java/com/example/Main.java
+++ srusty/java-servlet-crud-hibernate/src/main/java/com/example/Main.java
@@ -1,7 +1,17 @@
-package com.example;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import java.io.File;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class EmbeddedTomcat {
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        String webAppDir = new File("src/main/webapp/").getAbsolutePath();
+        tomcat.addWebapp("/", webAppDir);
+
+        System.out.println("Starting Embedded Tomcat on port 8080...");
+        tomcat.start();
+        tomcat.getServer().await();
     }
 }
